96ba54b6d281a45a4efb8603272e853e51216dd7
Varad-Kulkanri <36605907+Varad-Kulkanri@users.noreply.github.com>
2018-03-09 07:59:37 +0530

    Added Arduino master and Arduino slave code

---
+++ Ardino_Master/Ardino_Master.ino
@@ -0,0 +1,23 @@
+// Arduino as Master Code
+// by Varad Kulkarni <http://www.microcontrollershub.com>
+// Created 08 March 2018
+
+#include <Wire.h>
+
+void setup() 
+{
+  Wire.begin();                     // Initialize Arduino as Master
+}
+
+void loop() 
+{
+  Wire.beginTransmission(0x40);    // Transmit to device #0x40
+  Wire.write('0');                 // Sends one byte as '0'
+  Wire.endTransmission();          // End transmition
+  delay(2000);
+
+  Wire.beginTransmission(0x40);    // Transmit to device #0x40
+  Wire.write('1');                 // Sends one byte as '1'
+  Wire.endTransmission();          // End transmition
+  delay(2000);
+}
---
+++ Arduino_Slave/Arduino_Slave.ino
@@ -0,0 +1,39 @@
+// Arduino as Slave Code
+// by Varad Kulkarni <http://www.microcontrollershub.com>
+// Created 08 March 2018
+
+#include <Wire.h>
+
+void setup() 
+{
+  Wire.begin(0x40);                 // Initialize Arduino as slave with addres 0x40
+  Wire.onReceive(receiveEvent);     // Register event
+  Serial.begin(9600);               // Start serial for debug
+  pinMode(13,OUTPUT);               // Use LED connected tompin 13 as indicator
+  digitalWrite(13,LOW);             // Initialize LED in OFF state
+}
+
+void loop() 
+{
+}
+
+// function that executes whenever data is received from master
+
+void receiveEvent(int howMany) 
+{
+  while (0 < Wire.available())              // loop through all but the last
+  {  
+    Serial.println("data received"); 
+    char cReceivedData = Wire.read();       // receive byte as an char
+    Serial.println(cReceivedData);          // print the character
+    if('0' == cReceivedData)                // If '0' is received, turn LED OFF
+    {
+      digitalWrite(13,LOW);
+    }
+    else if('1' == cReceivedData)           // If '1' is received, turn LED ON
+    {
+      digitalWrite(13,HIGH);
+    }
+  }
+
+}
